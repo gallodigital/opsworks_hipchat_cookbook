200f79c9feea89fe7d4688a405304057de176495
John Schult <johnschult@users.noreply.github.com>
2014-06-09 16:15:09 -0400

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name 'opsworks-hipchat'
+description 'A cookbook for sending OpsWorks deploy event messages to HipChat'
+version "0.1.0"
+maintainer 'John Schult'
+maintainer_email 'john@schult.us'
+license 'MIT'
