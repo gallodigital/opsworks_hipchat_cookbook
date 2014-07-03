1ad343cbaca4dee8c227ddbe3a881a0227d04bca
Keith Gable <keith@gallodigital.com>
2014-07-03 15:58:12 -0500

    Update the metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name 'opsworks-hipchat'
+name 'opsworks_hipchat'
 description 'A cookbook for sending OpsWorks deploy event messages to HipChat'
 version "0.1.0"
-maintainer 'John Schult'
-maintainer_email 'john@schult.us'
+maintainer 'Keith Gable'
+maintainer_email 'keith@gallodigital.com'
 license 'MIT'
