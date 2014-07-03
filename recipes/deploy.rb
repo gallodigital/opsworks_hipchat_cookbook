c940012eb5fc770c837c36f372bed72284024e03
Keith Gable <keith@gallodigital.com>
2014-07-03 15:58:53 -0500

    Use the LWRP to send a notification and build a less stupid message.

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,15 +1,14 @@
 node[:deploy].each do |application, deploy|
-  token = deploy[:hipchat_token]
-  room = deploy[:hipchat_room_id]
-  sent_from = deploy[:hipchat_notify_from] || 'Amazon OpsWorks'
-  application_name = application
-  if deploy[:scm] and deploy[:deploying_user]
+  Chef::Log.info("Sending HipChat deployment notification for #{application}")
+
+  if deploy[:scm] && deploy[:deploying_user]
     scm = deploy[:scm]
     opsworks_user = deploy[:deploying_user].split('/')[1]
   else
     scm = nil
     opsworks_user = nil
   end
+
   if deploy[:hipchat_run_on]
     if deploy[:hipchat_run_on].to_s == '_all'
       perform_notification = true
@@ -20,14 +19,22 @@ node[:deploy].each do |application, deploy|
     perform_notification = false
   end
 
-  if token && room && application_name && perform_notification && !scm.nil? then
-    stack_state = JSON.parse(`opsworks-agent-cli stack_state`)
-    stack_name = stack_state['stack']['name']
-    stack_id = stack_state['stack']['id']
+  if token && room && application_name && perform_notification && !scm.nil?
+    stack_name = node['opsworks']['stack']['name']
+    stack_id = node['opsworks']['stack']['id']
 
-    execute "send hipchat notification" do
-      command "/usr/local/bin/hipchat_notification.py #{token} #{room} #{sent_from} #{application_name} #{opsworks_user} \"#{stack_name}\" #{stack_id}"
-      action :run
+    # Build the message to send to HipChat.
+    message = ""
+    message << "#{opsworks_user} " if opsworks_user
+    message << "successfully deployed #{application}.<br />"
+    message << "Stack: #{stack_name}<br />"
+    message << "Instance: #{node['hostname']}<br />"
+    message << "<a href=\"https://console.aws.amazon.com/opsworks/home?#/stack/#{stack_id}/deployments/#{node['opsworks']['deployment']}\">details &rdquo;</a>"
+
+    opsworks_hipchat_notification "post-deploy-notification" do
+      room deploy[:hipchat_room_id]
+      token deploy[:hipchat_token]
+      message message
     end
   end
 end
